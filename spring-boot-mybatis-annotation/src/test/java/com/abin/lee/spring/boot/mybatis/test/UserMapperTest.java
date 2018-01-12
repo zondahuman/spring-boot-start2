ea8c869a29e771ef92f96f37c0a2ef35e4de44d9
zondahuman <zondahuman@gmail.com>
2018-01-12 19:00:51 +0800

    update

--- spring-boot-mybatis-annotation/src/test/java/com/abin/lee/spring/boot/mybatis/test/UserMapperTest.java
+++ spring-boot-mybatis-annotation/src/test/java/com/abin/lee/spring/boot/mybatis/test/UserMapperTest.java
@@ -7,10 +7,13 @@ package com.abin.lee.spring.boot.mybatis.test;
  */
 import com.abin.lee.spring.boot.mybatis.enums.UserSexEnum;
 import com.abin.lee.spring.boot.mybatis.model.UserEntity;
+import net.minidev.json.JSONUtil;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonJsonParser;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.abin.lee.spring.boot.mybatis.mapper.UserMapper;
@@ -37,7 +40,9 @@ public class UserMapperTest {
     @Test
     public void testQuery() throws Exception {
         List<UserEntity> users = UserMapper.getAll();
-        System.out.println(users.toString());
+        System.out.println("---------------------------start------------------------: ");
+        System.out.println(JSONObject.valueToString(users));
+        System.out.println("---------------------------end------------------------: ");
     }
 
 
