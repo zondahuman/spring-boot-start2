b0bbd5cabc753e758cb5bdde4958ce54be7b4644
zondahuman <zondahuman@gmail.com>
2018-01-15 13:08:36 +0800

    add

---
+++ spring-boot-common/src/test/java/com/abin/lee/spring/boot/common/test/AppTest.java
@@ -0,0 +1,25 @@
+package com.abin.lee.spring.boot.common.test;
+
+/**
+ * Created by abin on 2018/1/15 11:35.
+ * spring-boot-start2
+ * com.abin.lee.spring.boot.common.test
+ */
+public class AppTest {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
