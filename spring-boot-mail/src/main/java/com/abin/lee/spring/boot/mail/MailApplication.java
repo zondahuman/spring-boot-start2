e650e6669daca5ac83ffe9c690904490dd2cfea4
zondahuman <zondahuman@gmail.com>
2018-01-16 19:58:19 +0800

    add

---
+++ spring-boot-mail/src/main/java/com/abin/lee/spring/boot/mail/MailApplication.java
@@ -0,0 +1,17 @@
+package com.abin.lee.spring.boot.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by abin on 2018/1/16 19:37.
+ * spring-boot-start2
+ * com.abin.lee.spring.boot.mail
+ */
+@SpringBootApplication
+public class MailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailApplication.class, args);
+    }
+}
