cc5008e7a8e2d4441ffb50a428dbc27fd175495a
zondahuman <zondahuman@gmail.com>
2018-01-17 19:39:48 +0800

    update

--- spring-boot-cassandra/src/main/java/com/abin/lee/spring/boot/cassandra/commands/ProductForm.java
+++ spring-boot-cassandra/src/main/java/com/abin/lee/spring/boot/cassandra/commands/ProductForm.java
@@ -1,6 +1,10 @@
 package com.abin.lee.spring.boot.cassandra.commands;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
@@ -8,7 +12,9 @@ import java.util.UUID;
 /**
  * Created by jt on 1/10/17.
  */
-
+@Setter
+@Getter
+@ToString
 public class ProductForm {
     private UUID id;
     private String description;
@@ -18,59 +24,5 @@ public class ProductForm {
     private Date createTime;
     private Date updateTime;
 
-    public UUID getId() {
-        return id;
-    }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public boolean isFlag() {
-        return flag;
-    }
-
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
--- spring-boot-cassandra/src/main/java/com/abin/lee/spring/boot/cassandra/domain/Product.java
+++ spring-boot-cassandra/src/main/java/com/abin/lee/spring/boot/cassandra/domain/Product.java
@@ -1,6 +1,9 @@
 package com.abin.lee.spring.boot.cassandra.domain;
 
 import com.datastax.driver.core.DataType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.cassandra.core.mapping.CassandraType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
@@ -11,10 +14,13 @@ import java.util.Date;
 import java.util.UUID;
 
 
+
 /**
  * Created by jt on 1/10/17.
  */
-
+@Setter
+@Getter
+@ToString
 @Table("products")
 public class Product implements Serializable{
 
@@ -28,63 +34,5 @@ public class Product implements Serializable{
     private Date createTime;
     private Date updateTime;
 
-    public UUID getId() {
-        return id;
-    }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Product() {
-        id = UUID.randomUUID();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public boolean isFlag() {
-        return flag;
-    }
-
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
 }
